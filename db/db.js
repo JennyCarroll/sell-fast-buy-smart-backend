8ce9b8f58219d5460fd4181062eb81ca64321633
Jenny Carroll <jennygc@telus.net>
2023-04-20 17:43:18 -0600

    put it all back

--- db/db.js
+++ db/db.js
@@ -5,19 +5,19 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 //took the original connection string and added on the root certificate DB_CA_CERT environment variable that in on Digital Ocean
-// const conString =
-//   process.env.DB_STRING + "&sslrootcert=" + process.env.DB_CA_CERT;
+const conString =
+  process.env.DB_STRING + "&sslrootcert=" + process.env.DB_CA_CERT;
 
 const dbParams = {
   // //this connects us directly to the Digital Ocean db
-  // connectionString: conString,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  connectionString: conString,
+  // host: process.env.DB_HOST,
+  // port: process.env.DB_PORT,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASS,
+  // database: process.env.DB_NAME,
   //set ssl to true for Digital Ocean
-  ssl: true,
+  // ssl: true,
   // user: 'labber',
   // password: 'labber'
 };
