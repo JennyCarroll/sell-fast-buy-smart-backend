17bf95c82b7920f4aa1fe8dd9f83cea19f8899c2
Jenny Carroll <jennygc@telus.net>
2023-04-20 16:46:34 -0600

    created a connection string using a variable in db.js

--- db/db.js
+++ db/db.js
@@ -5,13 +5,14 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const dbParams = {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DB_STRING,
+  // host: process.env.DB_HOST,
+  // port: process.env.DB_PORT,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASS,
+  // database: process.env.DB_NAME,
   //set ssl to true for Digital Ocean
-  ssl: true,
+  // ssl: true,
   // user: 'labber',
   // password: 'labber'
 };
