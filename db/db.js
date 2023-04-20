1da3852b4e13bc6e3f250c0536020f522e3b546d
Jenny Carroll <jennygc@telus.net>
2023-04-20 16:35:58 -0600

    update db for Digital Ocean

--- db/db.js
+++ db/db.js
@@ -1,15 +1,17 @@
 // load .env data into process.env
-require('dotenv').config();
+require("dotenv").config();
 
 // PG database client/connection setup
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const dbParams = {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  //set ssl to true for Digital Ocean
+  ssl: true,
   // user: 'labber',
   // password: 'labber'
 };
