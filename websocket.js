a74728ee3c857da5507ecd1114456b5078b71b11
Jason-Wall <jason.waldick@gmail.com>
2023-04-25 13:22:03 -0600

    update cors for websocket

--- websocket.js
+++ websocket.js
@@ -7,7 +7,11 @@ const socketBidNotify = (bid) => {
 };
 
 function startWebSocket(server) {
-  const io = socketio(server);
+  const io = socketio(server, {
+    cors: {
+      origin: 'https://starfish-app-bhxro.ondigitalocean.app/'
+    }
+  });
   ioInstance = io;
 
   // SOCKET LISTENER
