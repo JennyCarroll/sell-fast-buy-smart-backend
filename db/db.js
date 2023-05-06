1e70492779e1443cadb4d3e83810e452d1ddaa28
Jenny Carroll <jennygc@telus.net>
2023-05-06 15:30:53 -0600

    clean up db

--- db/db.js
+++ db/db.js
@@ -4,25 +4,15 @@ let fs = require("fs");
 const { Pool } = require("pg");
 
 const dbParams = {
-  // //this connects us directly to the Digital Ocean db
-  // connectionString:
-  //   "postgresql://doadmin:AVNS_aG9ulPC4xnym9pPn9_J@db-postgresql-tor1-25578-do-user-13968025-0.b.db.ondigitalocean.com:25060/defaultdb?sslmode=require",
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  //set ssl to true for Digital Ocean
+  //ssl for Digital Ocean
   ssl: {
     rejectUnauthorized: false,
   },
-  // ssl: {
-  //   ca_cert: fs.readFileSync("db/ca-certificate.crt").toString(),
-  // },
-  // ssl: true,
-  // sslValidate: false,
-  // user: 'labber',
-  // password: 'labber'
 };
 
 const db = new Pool(dbParams);
